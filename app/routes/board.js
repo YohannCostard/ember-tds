f6197d0841b7b715f9dd5a2ed7b857a9a46b363c
Yohann <22004112@etu.unicaen.fr>
2022-03-17 16:27:16 +0100

    Les trois quarts du TP

--- app/routes/board.js
+++ app/routes/board.js
@@ -1,11 +1,20 @@
-import Abstractroute from "./Abstractroute";
+import Abstractroute from './Abstractroute';
 import { action } from '@ember/object';
-
+import RSVP from 'rsvp';
 export default class BoardRoute extends Abstractroute {
-  model(){
-    return this.userAuth.getUser();
+  model() {
+    let user = this.userAuth.user;
+    if (user) {
+      return RSVP.hash({
+        orders: this.store.query('order', {
+          filter: { idEmployee: user.id },
+          include: 'orderdetails',
+        }),
+        employee: user,
+      });
+    }
   }
-  @action logout(){
+  @action logout() {
     this.userAuth.logout();
     this.refresh();
   }
